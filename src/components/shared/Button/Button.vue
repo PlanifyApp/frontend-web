a345606aeb6e7f563e89abaaf17219b90098c978
GustavoLS <106133669+GustavoLS1@users.noreply.github.com>
2025-07-20 14:17:51 -0500

    Prueba de Button

---
+++ src/components/shared/Button/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <Button label="" class="!tw-bg-primary tw-rounded tw-h-10 !tw-w-[100px] "/>
+  
+</template>
+
+<script setup lang="ts">
+import Button from 'primevue/button';
+
+const props = defineProps<{
+  label: string;
+  icon?: string;
+  iconPosition?: 'left' | 'right';
+  variant?: 'primary' | 'secondary';
+  disabled?: boolean;
+}>();
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,9 @@
   <div>
     alo buenas, me chupan la monda todos.
   </div>
+  <Button/>
 </template>
 
 <script lang="ts" setup>
-
+import Button from '@/components/shared/Button/Button.vue'
 </script>
