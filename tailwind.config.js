cc73e28fcd64ac2c65677362cf287195f304cb1b
GregoryG8 <gregory.gonzalez@correounivalle.edu.co>
2025-07-19 22:54:15 -0500

    feat: add Button component with props and styles; update HomeView to include buttons; add primeicons dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       colors: {
         primary: "#007DC3",
         secondary: "#464646",
+        'secondary-active' : "#4646461A",
         tertiary: "#F1632A",
         'primary-hover': "#007DC378",
         'dark-blue': "#0169A4",
