7a6bbde0c3061dd41ababb19e7d6e0fde55c34c1
Juan Hoyos <juanhoyos1347@gmail.com>
2025-07-23 22:36:00 -0500

    Feat: add component ToggleSwitch

---
+++ src/components/shared/Switch/Switch.vue
@@ -0,0 +1,41 @@
+<template>
+  <ToggleSwitch
+    v-model="model"
+    :disabled="props.disabled"
+    :class="{
+        'tw-bg-primary-500': model,
+    }"
+  />
+</template>
+
+<script setup lang="ts">
+import ToggleSwitch from 'primevue/toggleswitch';
+import { computed } from 'vue';
+import { SwitchProps } from './SwitchProps';
+import { useSwitch } from './useSwitch';
+
+const props = withDefaults(defineProps<SwitchProps>(), {
+  disabled: false,
+  type: 'primary',
+});
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void;
+}>();
+
+const model = computed({
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value),
+});
+
+
+
+const { switchClass } = useSwitch(props);
+const classes = switchClass;
+</script>
+
+<style>
+.p-inputswitch {
+  background-color: transparent !important;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
-<!-- Esta vista va a ser de pruebas para los componentes creados -->
+  <!-- Esta vista va a ser de pruebas para los componentes creados -->
   <div>
     alo buenas, me chupan la monda todos.
   </div>
+
   <div class="tw-w-56">
     <Button label="Guardar" type="primary" />
     <Button label="Cancelar" type="secondary" icon="times" icon-pos="right" />
@@ -12,17 +13,27 @@
   <div>
     EL de arriba me chupa la chupa
   </div>
+
   <div>
     <Checkbox v-model="checkbox1" label="Aceptar términos" type="primary" />
     <Checkbox v-model="checkbox2" label="Recibir correos" type="secondary" />
   </div>
+
+  <div class="tw-mt-4">
+    <Switch v-model="switchValue" type="primary" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref,watch } from 'vue';
 import Button from '@/components/shared/Button/Button.vue';
 import Checkbox from '@/components/shared/Checkbox/Checkbox.vue';
-import { ref } from 'vue';
+import Switch from '@/components/shared/Switch/Switch.vue';
 
 const checkbox1 = ref(false);
 const checkbox2 = ref(true);
+const switchValue = ref(false);
+watch(switchValue, (newValue) => {
+  console.log('Switch value changed:', newValue);
+});
 </script>
